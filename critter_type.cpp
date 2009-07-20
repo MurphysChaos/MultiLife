2d61b5cfc05264c11a6f7cdd82bbf97bcc4d57de
Joel Murphy <joel@oobie.(none)>
2009-07-20 08:18:57 -0700

    Coded some of critter_type.cpp.

--- critter_type.cpp
+++ critter_type.cpp
@@ -18,9 +18,58 @@
 CritterType::CritterType()
 {
     observe = new char[7];
+    for (int i=0;i++;i<7)
+	observe[i] = 0;
 }
 
 CritterType::~CritterType()
 {
     delete [] observe;
+}
+
+void setColor( QColor& newColor )
+{
+    this.color = newColor;
+}
+
+// Usage: setObserveCell( [-3,3], [-3,3], [true,false] );
+// x := horizontal offset from center (-3 to 3)
+// y := vertical offset from center (-3 to 3)
+// value := true to observe, false to ignore
+// Note: offset 0,0 is not used by the simulation
+void setObserveCell( int x, int y, bool value )
+{
+    char change = 2 ^ (x + 3);
+    char remain = 0xff - change;
+    this.observe[y] = (observe[y] & remain) + (change & ( value ? 0xff : 0x00 ) );
+}
+
+void setObserveOthers( bool value )
+{
+    this.observeOthers = value;
+}
+
+void setPushOut( bool value )
+{
+    this.pushOut = value;
+}
+
+void setMinSurvive( int value )
+{
+    this.minSurvive = value;
+}
+
+void setMaxSurvive( int value )
+{
+    this.maxSurvive = value;
+}
+
+void setMinCreate( int value )
+{
+    this.minCreate = value;
+}
+
+void setMaxCreate( int value )
+{
+    this.maxCreate = value;
 }
